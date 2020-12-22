308dba54e36942d8502a569fe13f84d502041398
adnan.drina@gmail.com <adnan.drina@gmail.com>
2020-12-22 16:54:02 +0100

    housekeeping

--- install.sh
+++ install.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 #Make sure you're connected to your OpenShift cluster with admin user before running this script
-
+echo " "
 echo "Creating CRW namespace"
 oc apply -f crw-namespace.yaml
 echo "CRW namespace created!"
-
+echo " "
 echo "Creating CRW OperatorGroup"
 oc apply -f crw-og.yaml
 echo "CRW OperatorGroup created!"
-
+echo " "
 echo "Creating CRW Subscription"
 oc apply -f crw-sub.yaml
 echo "CRW Subscription created!"
-wait 10
-
+echo " "
 echo "Deploying CheCluster CR"
 oc apply -f che-ephemeral-cr.yaml
 echo "CheCluster CR created!"
-
-echo "Searching for available routes"
-oc get routes -n codeready-workspaces
-echo "connect to the route named codeready using your browser \
-and login using your openshift credentials"
+echo " "
+#echo "Searching for available routes"
+#oc get routes -n codeready-workspaces
+#echo "connect to the route named codeready using your browser \
+#and login using your openshift credentials"
 
