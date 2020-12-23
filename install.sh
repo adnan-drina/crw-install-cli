7e7c44a1748a031257026d1b562ebfa78d1c62b1
adnan.drina@gmail.com <adnan.drina@gmail.com>
2020-12-23 10:09:58 +0100

    wait added

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ echo "CRW OperatorGroup created!"
 echo " "
 echo "Creating CRW Subscription"
 oc apply -f crw-sub.yaml
+CRW="$(oc get pods -o name -n codeready-workspaces | grep codeready-operator)"
+oc -n codeready-workspaces wait --for=condition=Ready ${CRW}
 echo "CRW Subscription created!"
 echo " "
 echo "Deploying CheCluster CR"
